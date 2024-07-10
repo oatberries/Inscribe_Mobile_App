1089f1cb009f7189a2f692ba0c0054798c63b936
Tabbatha Rivera <oatmealjunk@gmail.com>
2024-07-10 14:53:07 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:inscribevs/pages/login_page.dart';
+import 'package:inscribevs/pages/register_page.dart';
 
 void main() {
   //runApp(const MainApp());
-  runApp(const MyWidget());
+  runApp(const MyApp());
 }
-/*
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        //primarySwatch: Colors.blue;
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
-*/
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //this widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 187, 230, 189),
-        appBar: AppBar(
-          
-        //backgroundColor: Color.fromARGB(255, 187, 230, 189),
-        title: const Text('Inscribe'),
-        
-          
-          //decoration: BoxDecoration(backgroundBlendMode: context
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage()
+      //home: RegisterPage()
+     
       );
       
     
