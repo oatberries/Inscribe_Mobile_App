30a2cceaf6c5c22201771bd2f286075b44ebc558
Tabbatha Rivera <oatmealjunk@gmail.com>
2024-07-11 23:21:43 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:inscribevs/pages/login_page.dart';
-import 'package:inscribevs/pages/register_page.dart';
+import 'package:inscribevs/routes/routes.dart';
+//import 'package:inscribevs/screens/LoginScreen.dart';
+import 'package:inscribevs/screens/LoginPage.dart';
+import 'package:inscribevs/screens/register_page.dart';
+
 
 void main() {
   //runApp(const MainApp());
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //const MyApp({super.key});
 
   //this widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: '',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      //routes: Routes.getroutes,
       home: LoginPage()
       //home: RegisterPage()
      
