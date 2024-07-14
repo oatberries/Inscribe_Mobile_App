51ce1f40b2b45070310552807d486e4ed15c0538
Tabbatha Rivera <oatmealjunk@gmail.com>
2024-07-14 11:18:47 -0400

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,157 +1,153 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:inscribevs/components/my_forgotpasswordbutton.dart';
 import 'dart:convert';
 import 'package:inscribevs/components/my_loginbutton.dart';
+import 'package:inscribevs/components/my_signupbutton.dart';
 import 'package:inscribevs/components/my_logintextfield.dart';
-import 'package:inscribevs/pages/register_page.dart';
+import 'package:inscribevs/pages/home_page.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+//class LoginScreen extends StatefulWidget{
+class LoginPage extends StatefulWidget{
+  
+  const LoginPage({super.key});
 
-  //text editing controllers
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
+  @override
+  _LoginPageState createState() => _LoginPageState();
 
-  // sign user in
-  void signUserIn(){
+}
 
+class _LoginPageState extends State<LoginPage> {
 
+  //Holds the values types in by the user
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> _login() async{
+
+      final String username =  usernameController.text;
+      final String password = passwordController.text;
+
+      //API endpoint url
+      const String URL = 'https://inscribed-22337aee4c1b.herokuapp.com/api/auth/login';
+
+      //Call the API endpoint 
+      final response = await http.post(
+        Uri.parse(URL),
+        headers: <String, String> {
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: json.encode(<String, String>{
+          'username' : username,
+          'password': password
+        })
+      );
+
+      //if the response is ok
+      if(response.statusCode == 200){
+
+        Navigator.push(context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      
+        final responseData = jsonDecode(response.body);
+        print('Registration successful: ${responseData}');
+
+      }
+      else{
+        print('Registration failed: ${response.body}');
+      }
 
   }
 
+
   @override
   Widget build(BuildContext context) {
-    final bgcolor = Color(0xD8F3DC);
-    return Scaffold(
-     backgroundColor: Color.fromARGB(255, 216, 243, 220),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-        
-               // const SizedBox(height: 25),
-        
-              //logo 
-         /*     const Icon(
-                Icons.login,
-                   size: 100
-              ),
-        */
-              const SizedBox(height: 100),
-        
-              //Welcome back! Log in
-              const Text(
-                'Log In',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 40,
-                fontWeight: FontWeight.w400
-                )
-              ),
-        
-              const SizedBox(height: 15),
-        
-              //username text field
-              MyLoginTextField(
-                controller: usernameController,
-                hintText: 'Username',
-                obscureText: false
-              ),
-        
-               const SizedBox(height: 10),
-        
-              //password text field
-              MyLoginTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true
-              ),
-/*
-              const SizedBox(height: 5),
-              //forgot password?
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                     Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        color: Colors.black,
-                         fontSize: 12
-                         )
-                      ),
-                    ],
-                  ),
-                ),
-        */
-              const SizedBox(height: 10),
-              //Sign in button
-              //const SizedBox(height:10),
-              MyLoginButton(
-                onTap: signUserIn,
-              ),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+       backgroundColor: Color.fromARGB(255, 216, 243, 220),
+      // body: MainPage(),
+       body: SingleChildScrollView(
+          child: SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
 
-              const SizedBox(height:10),
-              //Forgot Password?
-       //      ElevatedButton(
-            /*    onPressed: () {
-                  Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ))
-                },*/
-                //child: 
-                const Text(
-                    'Forgot Password?',
+                  //Welcome back! Log in
+                  const SizedBox(height: 100),
+                  const Text(
+                    'Log In',
                     style: TextStyle(
-                     color: Colors.black,
-                     fontSize: 16,
-                     fontWeight: FontWeight.normal
-                  )
-                ),
-             // ),
-
-
-              const SizedBox(height: 2),
-              //Sign Up!
-                TextButton(
-                  onPressed: () {
-                  Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
-                },
-                  child: const Text(
-                  'Sign Up!',
-                  style: TextStyle(
-  
                     color: Colors.black,
-                     fontSize: 16
+                    fontSize: 40,
+                    fontWeight: FontWeight.w400
+                    )
+                  ),
 
-                     )
-                                ),
-                ),
+
+                  //username text field
+                  const SizedBox(height:20),
+                  MyLoginTextField(
+                      controller: usernameController,
+                      obscureText: false,
+                      hintText: 'username',
+                      isUsernameField: true,
+                      isPasswordField: false,            
+                  ),
+           
+           
+                  //password text field
+                  const SizedBox(height: 10),
+                  MyLoginTextField(
+                      controller: passwordController,
+                      obscureText: false,
+                      hintText: 'password',
+                      isUsernameField: false,
+                      isPasswordField: true,            
+                  ),
              
-              const SizedBox(height: 50),
-        /*
-              //or continue with
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                    thickness: 0.5,
-                  color: Colors.grey[500],
-                  ),
-                  ),
-                ],
-                )
-        */
+                  //Sign in button
+                  const SizedBox(height: 10),
+                  MyLoginButton(onPressed: _login),
             
-              ],
-            ),
+
+
+                  //Forgot Password?
+                  const SizedBox(height:10),
+                     //      ElevatedButton(
+                /*    onPressed: () {
+                      Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const ))
+                    },*/
+                    //child: 
+                    ForgotPasswordbutton(),
+                 /*   const Text(
+                        'Forgot Password?',
+                         style: TextStyle(
+                         color: Colors.black,
+                         fontSize: 16,
+                         fontWeight: FontWeight.normal
+                      )
+                    ),*/
+                 // ),
+              
+
+
+                  //sign up button
+                  //const SizedBox(height: 2),
+                  Signupbutton(),
+               
+                  ],
+                ),
+              ),
+           
           ),
-        ),
-      )
+        )
+      ),
     );
   }
 }
