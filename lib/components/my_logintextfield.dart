960bae39e47a5a6c7a029144afe91712eca76e29
Tabbatha Rivera <oatmealjunk@gmail.com>
2024-07-14 11:27:21 -0400

    Update my_logintextfield.dart

--- lib/components/my_logintextfield.dart
+++ lib/components/my_logintextfield.dart
@@ -4,39 +4,51 @@ class MyLoginTextField extends StatelessWidget {
   final controller;
   final String hintText;
   final bool obscureText;
+  final bool isUsernameField;
+  final bool isPasswordField;
 
   const MyLoginTextField({
       super.key,
       required this.controller,
       required this.hintText,
-      required this.obscureText
+      required this.obscureText,
+      required this.isUsernameField,
+      required this.isPasswordField
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: TextField(
-                controller: controller,
-                obscureText: obscureText,
-                decoration: InputDecoration(
-                 
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Colors.grey),
+    return SizedBox(
+      height: 45,
+      child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextFormField(
+                  controller: controller,
+                  obscureText: obscureText,
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter a $hintText';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                   
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: Color.fromARGB(255, 252, 252, 252)),
+                    ),
+                    
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: hintText,
+                    //hintStyle: TextStyle(color: Colors.)
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: Color.fromARGB(255, 252, 252, 252)),
-                  ),
-                  
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: hintText,
-                  //hintStyle: TextStyle(color: Colors.)
                 ),
-              ),
+      ),
     );
   }
 }
-
