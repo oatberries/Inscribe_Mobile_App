bbb71fc16db202e2437b2fc278ff96afb12e8127
Tabbatha Rivera <oatmealjunk@gmail.com>
2024-07-14 11:26:31 -0400

    Update my_loginbutton.dart

--- lib/components/my_loginbutton.dart
+++ lib/components/my_loginbutton.dart
@@ -1,37 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:inscribevs/screens/home_page.dart';
+
 class MyLoginButton extends StatelessWidget {
 
   //final Function()? onTap;
   final VoidCallback onPressed;
 
- 
-
   const MyLoginButton({required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
-      padding: const EdgeInsets.all(10),
+      height: 45,
+
       margin: const EdgeInsets.symmetric(horizontal: 50),
-       decoration: BoxDecoration(color: Color.fromRGBO(82,183,136,1),
-           borderRadius: BorderRadius.circular(8),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(82,183,136,1),
+        borderRadius: BorderRadius.circular(8),
        ),
-        child: ElevatedButton(
-          onPressed: (){
-            Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          },
-          child: const Center(       
-            child: Text(
-            "Sign In",
-            style: TextStyle(color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+        child: TextButton(
+           style: TextButton.styleFrom(backgroundColor: Color.fromRGBO(82,183,136,1)),
+           onPressed: onPressed,
+  
+           child: const Center(   
+            
+                child: Text(
+                  "Sign In",
+                    style: TextStyle( 
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                ),
+            
             ),
-            ),
+
           ),
+
         )
     );
                 
